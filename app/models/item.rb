36e5ba342b549e6b92bd1e56df4ef56f5acc8b25
Reeta Olivier <reeta_olivier@hotmail.co.uk>
2023-03-22 10:47:12 -0300

    Ocassion array

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   has_many_attached :photos
 
   COLOURS = %w[Multi Black White Green Pink Blue Red Brown Yellow Orange Purple Navy Grey Gold Silver]
-  OCCASIONS = %w[Wedding]
+  OCCASIONS = %w[Wedding Festival Workwear Modest Partywear Summer Winter Other]
   CATEGORIES = %w[Coats Jackets Knitwear Dresses Tracksuits Tops Shirts Shorts Trousers Denim Sportswear Suits Skirt Swimwear Bag Shoes Jewellery]
   CONDITIONS = ["Past its best", "Used but good", "Like new"]
   validates_presence_of :size, :colour, :occasion, :category, :brand, :condition, :description, :photos
