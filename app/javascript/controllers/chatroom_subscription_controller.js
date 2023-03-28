fcc2521a9f69c87f82f485bc120c707a5b59348c
Reeta Olivier <reeta_olivier@hotmail.co.uk>
2023-03-28 16:07:20 -0300

    reinstate flatpckr and chatroom

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,29 +1,29 @@
-// import { Controller } from "@hotwired/stimulus"
-// import { createConsumer } from "@rails/actioncable"
+import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
 
-// export default class extends Controller {
-//   static values = { chatroomId: Number }
-//   static targets = ["messages"]
+export default class extends Controller {
+  static values = { chatroomId: Number }
+  static targets = ["messages"]
 
-//   connect() {
-//     this.channel = createConsumer().subscriptions.create(
-//       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-//       { received: data => this.#insertMessageAndScrollDown(data) }
-//     )
-//     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
-//   }
+  connect() {
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => this.#insertMessageAndScrollDown(data) }
+    )
+    console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
 
-//   disconnect() {
-//     console.log("Unsubscribed from the chatroom")
-//     this.channel.unsubscribe()
-//   }
+  disconnect() {
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe()
+  }
 
-//   #insertMessageAndScrollDown(data) {
-//     this.messagesTarget.insertAdjacentHTML("beforeend", data)
-//     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
-//   }
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
 
-//   resetForm(event) {
-//     event.target.reset()
-//   }
-// }
+  resetForm(event) {
+    event.target.reset()
+  }
+}
