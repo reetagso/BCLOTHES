5d3f981438f53c06908f980d41c1344ca1585930
Reeta Olivier <reeta_olivier@hotmail.co.uk>
2023-03-29 16:17:13 -0300

    landing page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get 'items/index'
   # get 'items/show'
   devise_for :users
-  root to: 'items#index'
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
